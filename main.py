a382d91377f10bd02db21b9a239300c319377ad5
Kaspar Moberg <kaspar@kasparm.home>
2024-05-05 18:22:04 +0200

    added downsampling of solutions so they have same length

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
+from sklearn.decomposition import PCA
 
 matplotlib.use("TkAgg")
 
@@ -35,8 +36,30 @@ ys = np.repeat(-2.0, 20)
 y0s = [[x, y] for x, y in zip(xs, ys)]
 
 solutions = [solve_ivp(vector_field, (0, 10), y0, method="RK45", t_eval=t) for y0 in y0s]
+solutions = [solution.y for solution in solutions]
 
+min_len = min(len(solution[0]) for solution in solutions)
+
+downsampled_solutions = []
 for solution in solutions:
-    plt.plot(solution.y[0], solution.y[1])
+    downsampled_solution = [[], []]
+    solution_length = len(solution[0])
+    step = solution_length / min_len
+    i = 0
+
+    while round(i) < solution_length:
+        downsampled_solution[0].append(solution[0][round(i)])
+        downsampled_solution[1].append(solution[1][round(i)])
+        i += step
+
+    downsampled_solutions.append(downsampled_solution)
+
+
+for solution in downsampled_solutions:
+    plt.plot(solution[0], solution[1])
+
+pca = PCA(n_components=2)
+
+#principalComponents = pca.fit_transform(solutions)
 
 plt.show()
