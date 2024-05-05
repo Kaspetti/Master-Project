2fadc191083e09ff66f6bfc9cdc3eb6b9c40de7c
Kaspar Moberg <kaspar@kasparm.home>
2024-05-05 15:43:07 +0200

    added integration of paths to the vector field

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.integrate import odeint
 
 matplotlib.use("TkAgg")
 
 
-# Define the vector field function F(x, y) = (-y, x)
-def vector_field(x, y):
-    return (2*x+y, x-y)
+def vector_field(y, t):
+    x, y = y
+    dx_dt = 2*x+y
+    dy_dt = x-y
+
+    return [dx_dt, dy_dt]
 
 
-# Define the grid of points
 x = np.linspace(-2, 2, 10)
 y = np.linspace(-2, 2, 10)
 X, Y = np.meshgrid(x, y)
 
-# Evaluate the vector field at each point in the grid
-U, V = vector_field(X, Y)
+U, V = vector_field((X, Y), 0)
 
-# Plot the flow field
-plt.figure(figsize=(8, 8))
 plt.quiver(X, Y, U, V, scale=20)
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Flow Field: F(x, y) = (-y, x)')
+plt.title('Flow Field: F(x, y) = (2*x+y, x-y)')
 plt.xlim(-2, 2)
 plt.ylim(-2, 2)
 plt.grid(True)
+
+t = np.linspace(0, 10, 100)
+xs = np.linspace(0.6, 0.65, 20)
+ys = np.repeat(-2.0, 20)
+
+y0s = [[x, y] for x, y in zip(xs, ys)]
+
+solutions = [odeint(vector_field, y0, t) for y0 in y0s]
+
+for solution in solutions:
+    x_solution, y_solution = solution[:, 0], solution[:, 1]
+    plt.plot(x_solution, y_solution)
+
 plt.show()
