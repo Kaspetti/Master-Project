b373508bb9054bb47842ff9652552188a6907374
Kaspar Moberg <kaspeti123@gmail.com>
2024-05-06 23:46:52 +0200

    added comments to code

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from scipy.integrate import solve_ivp
 matplotlib.use("TkAgg")
 
 
+# The function of the flow field
 def vector_field(t, y):
     x, y = y
     dx_dt = 2*x + y**2
@@ -13,14 +14,7 @@ def vector_field(t, y):
 
     return [dx_dt, dy_dt]
 
-
-x = np.linspace(-3, 5, 10)
-y = np.linspace(-2, 5, 10)
-X, Y = np.meshgrid(x, y)
-
-U, V = vector_field(0, (X, Y))
-
-plt.quiver(X, Y, U, V, scale=200)
+# Plot settings
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Flow Field: F(x, y) = (2*x + y**2, x**2 - y)')
@@ -28,6 +22,15 @@ plt.xlim(-3, 5)
 plt.ylim(-2, 5)
 plt.grid(True)
 
+# Plot arrows showing the direction of the flow field
+x = np.linspace(-3, 5, 10)
+y = np.linspace(-2, 5, 10)
+X, Y = np.meshgrid(x, y)
+U, V = vector_field(0, (X, Y))
+plt.quiver(X, Y, U, V, scale=200)
+
+
+# Integrate over the flow field using the Runge-Kutta method
 t = np.linspace(0, 10, 1000)
 xs = np.linspace(-0.9, -0.85, 20)
 ys = np.repeat(-2.0, 20)
@@ -37,8 +40,8 @@ y0s = [[x, y] for x, y in zip(xs, ys)]
 solutions = [solve_ivp(vector_field, (0, 10), y0, method="RK45", t_eval=t) for y0 in y0s]
 solutions = [solution.y for solution in solutions]
 
+# Downsample the solutions in order to get lines of the same dimension
 min_len = min(len(solution[0]) for solution in solutions)
-
 downsampled_solutions = []
 for solution in solutions:
     downsampled_solution = [[], []]
@@ -53,7 +56,7 @@ for solution in solutions:
 
     downsampled_solutions.append(downsampled_solution)
 
-
+# Plot the downsampled solutions
 for solution in downsampled_solutions:
     plt.plot(solution[0], solution[1])
 
