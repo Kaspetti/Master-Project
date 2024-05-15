90d2f3c17335208735af94ceaab0f9c2599e01a6
Kaspar Moberg <kaspeti123@gmail.com>
2024-05-15 17:41:54 +0200

    changed to interpolation from downsampling

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ matplotlib.use("TkAgg")
 
 
 # The function of the flow field
-def vector_field(t, y):
-    x, y = y
+def vector_field(t, ys, vector=False):
+    x, y = ys
     dx_dt = 2*x + y**2
     dy_dt = x**2 - y
 
+    if not vector and (abs(x) > 5 or abs(y) > 5):
+        return [0, 0]
+
     return [dx_dt, dy_dt]
+
 
 x = np.linspace(-3, 5, 10)
 y = np.linspace(-2, 5, 10)
 X, Y = np.meshgrid(x, y)
 
-U, V = vector_field(0, (X, Y))
+U, V = vector_field(0, (X, Y), vector=True)
 
 figure, axis = plt.subplots(1, 2)
 
@@ -33,7 +37,7 @@ axis[0].set_xlim(-3, 5)
 axis[0].set_ylim(-2, 5)
 axis[0].grid(True)
 
-t = np.linspace(0, 5, 1000)
+t = np.linspace(0, 10, 1000)
 xs = np.linspace(-0.95, -0.85, 50)
 ys = np.repeat(-2.0, 50)
 
@@ -42,30 +46,30 @@ y0s = [[x, y] for x, y in zip(xs, ys)]
 solutions = [solve_ivp(vector_field, (0, 10), y0, method="RK45", t_eval=t) for y0 in y0s]
 solutions = [solution.y for solution in solutions]
 
-# Downsample the solutions in order to get lines of the same dimension
-min_len = min(len(solution[0]) for solution in solutions)
-downsampled_solutions = []
+# Interpolate the lines to make them equal length
+point_amount = 300
+interpolated_solutions = []
 for solution in solutions:
-    downsampled_solution = [[], []]
-    solution_length = len(solution[0])
-    step = solution_length / min_len
-    i = 0
+    x = solution[0]
+    y = solution[1]
 
-    while round(i) < solution_length:
-        downsampled_solution[0].append(solution[0][round(i)])
-        downsampled_solution[1].append(solution[1][round(i)])
-        i += step
+    indices = np.arange(len(x))
+    new_indices = np.linspace(0, len(x) - 1, point_amount)
 
-    downsampled_solutions.append(downsampled_solution)
+    interp_x = np.interp(new_indices, indices, x)
+    interp_y = np.interp(new_indices, indices, y)
+
+    interpolated_line = np.vstack((interp_x, interp_y))
+    interpolated_solutions.append(interpolated_line)
 
 
 # PCA Test
-reshaped_data = np.reshape(downsampled_solutions, (50, -1))
+reshaped_data = np.reshape(interpolated_solutions, (50, -1))
 
 pca = PCA(n_components=2)
 projected_data = pca.fit_transform(reshaped_data)
 
-kmeans = KMeans(n_clusters=3)
+kmeans = KMeans(n_clusters=2)
 kmeans.fit(projected_data)
 
 axis[1].scatter(projected_data[:, 0], projected_data[:, 1], c=kmeans.labels_)
@@ -73,10 +77,16 @@ axis[1].set_xlabel('Principal Component 1')
 axis[1].set_ylabel('Principal Component 2')
 axis[1].set_title('Scatter Plot of Projected Lines')
 
-
-colors = ["blue", "red", "green"]
+colors = ["blue", "red", "orange"]
 # Plot the downsampled solutions
-for i, solution in enumerate(downsampled_solutions):
+for i, solution in enumerate(interpolated_solutions):
     axis[0].plot(solution[0], solution[1], color=colors[kmeans.labels_[i]])
+
+
+# for center in kmeans.cluster_centers_:
+#     center_line = center @ pca.components_
+#     center_line = np.reshape(center_line, (point_amount, 2))
+#     axis[0].plot(center_line[:, 0], center_line[:, 1], color="orange", linewidth=5)
+
 
 plt.show()
