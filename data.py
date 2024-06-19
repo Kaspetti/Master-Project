64dda8047b4a7a0fbd61e5cb515f852676785449
Kaspar Moberg <kaspeti123@gmail.com>
2024-06-20 01:06:38 +0200

    implemented visualization using d3 and leaflet

--- data.py
+++ data.py
@@ -2,13 +2,17 @@ from netCDF4 import Dataset
 
 
 def get_coords(line_id, time):
-    rootgrp = Dataset("./ec.ensctrl.2024051112.sfc.mta.nc", "r")
+    rootgrp = Dataset("./ec.ensctrl.2024061912.sfc.mta.nc", "r")
 
-    coords = [{"latitude": float(rootgrp["latitude"][i]),
-               "longitude": float(rootgrp["longitude"][i])}
-              for i in rootgrp["index"]
-              if rootgrp["line_id"][i] == line_id
-              and rootgrp["date"][i] == time]
+    line_id_indices = rootgrp["line_id"][:] == line_id
+    time_indices = rootgrp["date"][:] == time
+    matched_indices = line_id_indices & time_indices
+
+    latitudes = rootgrp["latitude"][matched_indices]
+    longitudes = rootgrp["longitude"][matched_indices]
+
+    coords = [{"latitude": float(lat), "longitude": float(lon)}
+              for lat, lon in zip(latitudes, longitudes)]
 
     rootgrp.close()
 
