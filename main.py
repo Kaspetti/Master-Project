bdc6a9cbb9d1a236d8b1cfd62570f8aec83cfb1b
Kaspar Moberg <kaspeti123@gmail.com>
2024-08-21 14:05:08 +0200

    added colors by id

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from data import get_all_lines_1
 import folium
+import random
+from matplotlib import colormaps as cm
+from matplotlib.colors import rgb2hex
 
 attr = (
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
@@ -11,12 +14,21 @@ tiles = "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png"
 lat = 0
 lon = 0
 zoom_start = 2
-m = folium.Map(location=[lat, lon], tiles=tiles,
-               attr=attr, zoom_start=zoom_start)
+m = folium.Map(location=[lat, lon],
+               tiles=tiles,
+               attr=attr,
+               zoom_start=zoom_start,
+               world_copy_jump=True)
 
 lines = get_all_lines_1(0)
+colormap = cm.get_cmap("tab10")
 
 for line in lines:
-    folium.PolyLine(line["coords"], weight=1, smooth_factor=0).add_to(m)
+    folium.PolyLine(
+            line["coords"],
+            weight=1,
+            smooth_factor=0,
+            color=rgb2hex(colormap((line["id"]-1) % 10))
+    ).add_to(m)
 
 m.save("index.html")
