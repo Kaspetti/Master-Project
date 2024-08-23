f4df568c263e98d84f058b9bd47912b8e187e437
Kaspar Moberg <kaspeti123@gmail.com>
2024-08-24 00:35:43 +0200

    added docstrings to functions

--- data_handling/multiscale.py
+++ data_handling/multiscale.py
@@ -6,24 +6,50 @@ import numpy as np
 
 
 def to_lat_lon(v):
+    '''
+    Converts a 3D coordinate into spherical coordinates (lat/lon)
+
+    Parameters
+    ----------
+    v : the (x, y, z) coordinates to convert to lat/lon.
+        Given as an array of size 3
+
+    Returns
+    -------
+    lat_lon : the [lat, lon] coordinates of v
+    '''
     lat = math.degrees(math.asin(v[2]))
     lon = math.degrees(math.atan2(v[1], v[0]))
 
     return [lat, lon]
 
 
-def get_all_lines():
+def get_all_lines(start, time_offset):
+    '''
+    Gets all the lines from a NetCDF file given the start date and time offset
+
+    Parameters
+    ----------
+    start : the start time of the simulation. Format: 'YYYYMMDDHH'.
+        Format important to be consistent with file names
+    time_offset : the time offset of the lines from the start time.
+        Given as hours from start time
+
+    Returns
+    -------
+    all_lines : an array of line objects. Line objects has "id" and an array
+        "coords" containing all [lat, lon] points
+    '''
     all_lines = []
 
-    start_time = np.datetime64("2024-08-23T00:00:00")
-    current_time = 3
+    start_time = np.datetime64(f"{start[0:4]}-{start[4:6]}-{start[6:8]}T{start[8:10]}:00:00")
 
     for i in range(50):
         ds = xr.open_dataset(
-                f"2024082300/ec.ens_{i:02d}.2024082300.sfc.mta.nc"
+                f"{start}/ec.ens_{i:02d}.{start}.sfc.mta.nc"
             )
         date_ds = ds.where(
-                    ds.date == start_time + np.timedelta64(current_time, "h"),
+                    ds.date == start_time + np.timedelta64(time_offset, "h"),
                     drop=True
                 )
 
@@ -46,6 +72,20 @@ def get_all_lines():
 
 
 def dateline_fix(coords):
+    '''
+    Shifts a list of coordinates by 360 in longitude
+
+    Note: This does not check if the coordinates cross the dateline,
+    it simply shifts them
+
+    Parameters
+    ----------
+    coords : the coordinates to shift. An array of [lat, lon]
+
+    Returns
+    -------
+    shifted_coords : the original coordinates shifted by 360 in latitude
+    '''
     for i in range(len(coords)):
         coord = coords[i]
         if coord[1] < 0:
@@ -55,10 +95,40 @@ def dateline_fix(coords):
 
 
 def get_enclosing_triangle(line_point, ico_points):
+    '''
+    Gets the indices of the vertices on the icosphere forming a
+    triangle enclosing the line point
+
+    Parameters
+    ----------
+    line_point : the point to get the enclosing triangle of ([lat, lon])
+    ico_points : all the vertices of the ico sphere ([[lat, lon]...])
+
+    Returns
+    -------
+    closest : the indices of the 3 closest points on the icosphere to the
+        line point. These 3 points form the triangle enclosing the point
+    '''
     dist_sqrd = np.sum((ico_points - line_point)**2, axis=1)
     sort_indices = np.argsort(dist_sqrd)
 
     return sort_indices[:3]
+
+
+def subdivide_triangle(points):
+    '''
+    Subdivides a triangle once creating three new points, one
+    on the midpoint on all three sides of the triangle
+
+    Parameters
+    ----------
+    points : the points making up the triangle
+
+    Returns
+    -------
+    new_points : 3 new points which subdivides the original triangle
+    '''
+    pass
 
 
 if __name__ == "__main__":
@@ -71,8 +141,8 @@ if __name__ == "__main__":
         ico_lat_lons.append(to_lat_lon(v))
 
     # read data
-    lines = get_all_lines()
-    print(get_enclosing_triangle(lines[0]["coords"][0], ico_lat_lons))
+    lines = get_all_lines("2024082300", 0)
+    # print(get_enclosing_triangle(lines[0]["coords"][0], ico_lat_lons))
 
     # show map
     attr = (
@@ -108,6 +178,30 @@ if __name__ == "__main__":
             weight=0,
             fill_opacity=1,
             fill=True,
+        ).add_to(m)
+
+    # enclosing triangle test
+    line_id = 5
+
+    for coord in lines[line_id]["coords"]:
+        enc_tri = get_enclosing_triangle(coord, ico_lat_lons)
+        for tri in enc_tri:
+            folium.CircleMarker(
+                location=ico_lat_lons[tri],
+                color="black",
+                weight=0,
+                fill_opacity=1,
+                fill=True,
+                radius=5,
+            ).add_to(m)
+
+        folium.CircleMarker(
+            location=coord,
+            color="green",
+            weight=0,
+            fill_opacity=1,
+            fill=True,
+            radius=5,
         ).add_to(m)
 
     m.save("index.html")
