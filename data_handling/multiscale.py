1fd7b7fb58f3c9e1df17d728a6fdb8c3918efd1f
Kaspar Moberg <kaspar@kasparm.home>
2024-09-28 23:42:53 +0200

    feat: switched to image generation as opposed to web based

--- data_handling/multiscale.py
+++ data_handling/multiscale.py
@@ -2,12 +2,12 @@ from icosphere import icosphere
 import math
 import xarray as xr
 import numpy as np
-import geoplot as gplt
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import LineString
 import matplotlib.pyplot as plt
-import contextily as cx
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
 
 def to_lat_lon(v):
@@ -172,137 +172,33 @@ if __name__ == "__main__":
     nu = 4
     ico_vertices, faces = icosphere(nu)
 
-    # read data
-    lines = get_all_lines("2024082300", 0)
+    i = 0
+    while (i <= 240):
+        print(f"Generating 2024082300_{i}h.svg")
+        lines = get_all_lines("2024082300", i)
 
-    ids = [line["id"] for line in lines]
-    df = pd.DataFrame(ids, columns=["id"])
-    geometry = [LineString(np.flip(line["coords"])) for line in lines]
-    gdf = gpd.GeoDataFrame(df, geometry=geometry, crs="EPSG:4326")
+        ids = [line["id"] for line in lines]
+        df = pd.DataFrame(ids, columns=["id"])
+        geometry = [LineString(np.flip(line["coords"])) for line in lines]
+        gdf = gpd.GeoDataFrame(df, geometry=geometry, crs="EPSG:4326")
 
-    ax = gdf.plot(figsize=(10, 10))
-    # cx.add_basemap(ax, crs="EPSG:4326")
+        fig = plt.figure(figsize=(12, 8))
+        ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
 
-    ax.set_xlim(-180, 180)
-    ax.set_ylim(-90, 90)
+        ax.add_feature(cfeature.LAND, facecolor="white", edgecolor="black")
+        ax.add_feature(cfeature.OCEAN, facecolor="lightgrey")
+        ax.add_feature(cfeature.COASTLINE, edgecolor="black")
+        ax.add_feature(cfeature.BORDERS, linestyle=':', edgecolor="darkgrey")
 
-    plt.xlabel('Longitude')
-    plt.ylabel('Latitude')
-    plt.title('2024082300 +0h all 50 members')
+        gdf.plot(ax=ax, transform=ccrs.PlateCarree())
 
-    plt.show()
+        ax.set_global()
+        ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
 
+        plt.savefig(f"./images/2024082300_{i}h.svg", transparent=True, format="svg")
+        plt.close()
 
-    # plt.savefig("plot.png")
-
-    # show map
-    # attr = (
-    #     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
-    #     'contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
-    # )
-    # tiles = "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png"
-    #
-    # lat = 0
-    # lon = 0
-    # zoom_start = 2
-    # m = folium.Map(location=[lat, lon],
-    #                tiles=tiles,
-    #                attr=attr,
-    #                zoom_start=zoom_start)
-    #
-    # # show the lines
-    # for line in lines:
-    #     folium.PolyLine(
-    #         locations=line["coords"],
-    #         weight=1,
-    #         color="#9999",
-    #         tooltip=line["id"],
-    #     ).add_to(m)
-    #
-    # # show the vertices of the icosphere on the map
-    # for i, v in enumerate(ico_vertices):
-    #     folium.CircleMarker(
-    #         location=to_lat_lon(v),
-    #         radius=2,
-    #         color="blue",
-    #         weight=0,
-    #         fill_opacity=1,
-    #         fill=True,
-    #         tooltip=i,
-    #     ).add_to(m)
-    #
-    # line_id = 5
-    # level = 5
-    #
-    # line_points = lines[line_id]["coords"]
-    # line_points_3d = [to_xyz(coord) for coord in lines[line_id]["coords"]]
-    #
-    # # for i in range(27, 28):
-    # for i in range(len(line_points)):
-    #     point_lat_lon = line_points[i]
-    #     point_3d = line_points_3d[i]
-    #
-    #     folium.CircleMarker(
-    #         location=point_lat_lon,
-    #         color="green",
-    #         weight=0,
-    #         fill_opacity=1,
-    #         fill=True,
-    #         tooltip=i,
-    #         radius=5,
-    #     ).add_to(m)
-    #
-    #     local_ico_points = ico_vertices
-    #     for i in range(level):
-    #         tri_pts_3d = get_enclosing_triangle(point_3d, local_ico_points)
-    #         subdiv_pts = subdivide_triangle(tri_pts_3d)
-    #
-    #         for pt in subdiv_pts:
-    #             folium.CircleMarker(
-    #                 location=to_lat_lon(pt),
-    #                 color="blue",
-    #                 weight=0,
-    #                 fill_opacity=1,
-    #                 fill=True,
-    #                 radius=2,
-    #             ).add_to(m)
-    #
-    #         # if i == level - 1:
-    #         #     for pt in local_ico_points:
-    #         #         print(np.linalg.norm(pt))
-    #         #         folium.CircleMarker(
-    #         #             location=to_lat_lon(pt),
-    #         #             color="black",
-    #         #             weight=0,
-    #         #             fill_opacity=1,
-    #         #             fill=True,
-    #         #             radius=5,
-    #         #         ).add_to(m)
-    #         #
-    #         #     for pt in subdiv_pts:
-    #         #         folium.CircleMarker(
-    #         #             location=to_lat_lon(pt),
-    #         #             color="red",
-    #         #             weight=0,
-    #         #             fill_opacity=1,
-    #         #             fill=True,
-    #         #             radius=3,
-    #         #         ).add_to(m)
-    #         #
-    #         #     for pt in tri_pts_3d:
-    #         #         folium.CircleMarker(
-    #         #             location=to_lat_lon(pt),
-    #         #             color="orange",
-    #         #             weight=0,
-    #         #             fill_opacity=1,
-    #         #             fill=True,
-    #         #             radius=3,
-    #         #         ).add_to(m)
-    #
-    #
-    #
-    #
-    #
-    #         local_ico_points = np.vstack((tri_pts_3d, subdiv_pts))
-    #
-    # m.save("index.html")
+        if i < 72:
+            i += 3
+        else:
+            i += 6
