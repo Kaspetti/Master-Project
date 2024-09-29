d3a4cc7019f5d274c076b822963a4665ad28d3cc
Kaspar Moberg <kaspeti123@gmail.com>
2024-09-29 15:33:37 +0200

    docs: added type hints and changed docstrings

--- data_handling/multiscale.py
+++ data_handling/multiscale.py
@@ -8,20 +8,27 @@ from shapely.geometry import LineString, Point
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+from typing import List, TypedDict
 
 
-def to_lat_lon(v):
+class Line(TypedDict):
+    id: str
+    coords: List[List[float]]
+
+
+def to_lat_lon(v: List[float]) -> List[float]:
     '''
     Converts a 3D coordinate into spherical coordinates (lat/lon)
 
     Parameters
     ----------
-    v : the (x, y, z) coordinates to convert to lat/lon.
-        Given as an array of size 3
+    v : List[float] of shape (3,)
+        The 3D (x, y, z) coordinates to convert to lat/lon.
 
     Returns
     -------
-    lat_lon : the [lat, lon] coordinates of v
+    coord : List[float] of shape (2,)
+        The lat/lon coordinates of v
     '''
     lat = math.degrees(math.asin(v[2]))
     lon = math.degrees(math.atan2(v[1], v[0]))
@@ -29,19 +36,20 @@ def to_lat_lon(v):
     return [lat, lon]
 
 
-def to_xyz(v):
+def to_xyz(v: List[float]) -> List[float]:
     '''
-    Converts a [lat, lon] coordinate into 3D coordinates ([x, y ,z]) on the
+    Converts from spherical coordinates into 3D coordinates (x, y ,z) on the
     unit sphere
 
     Parameters
     ----------
-    v : the [lat, lon] coordinate to convert
+    v : List[float] of shape (2,)
+        The spherical coordinate (lat/lon) to convert
 
     Returns
     -------
-    [x, y, z] : the coordinate represented as [x, y, z] coordinates
-    on the unit sphere
+    coord : List[float] of shape (3,)
+        The coordinate represented as [x, y, z] coordinates on the unit sphere
     '''
 
     x = math.cos(math.radians(v[0])) * math.cos(math.radians(v[1]))
@@ -51,21 +59,24 @@ def to_xyz(v):
     return [x, y, z]
 
 
-def get_all_lines(start, time_offset):
+def get_all_lines(start: str, time_offset: int) -> List[Line]:
     '''
     Gets all the lines from a NetCDF file given the start date and time offset
 
     Parameters
     ----------
-    start : the start time of the simulation. Format: 'YYYYMMDDHH'.
+    start : str
+        The start time of the simulation. Format: 'YYYYMMDDHH'.
         Format important to be consistent with file names
-    time_offset : the time offset of the lines from the start time.
+    time_offset : int
+        The time offset of the lines from the start time.
         Given as hours from start time
 
     Returns
     -------
-    all_lines : an array of line objects. Line objects has "id" and an array
-        "coords" containing all [lat, lon] points
+    all_lines : List[Line] of shape (n,) with n = line amount
+        An array of line objects. Line objects has "id" and an array
+        "coords" containing all lat/lon points
     '''
     all_lines = []
 
@@ -98,7 +109,7 @@ def get_all_lines(start, time_offset):
     return all_lines
 
 
-def dateline_fix(coords):
+def dateline_fix(coords: List[float]) -> List[float]:
     '''
     Shifts a list of coordinates by 360 in longitude
 
@@ -107,11 +118,13 @@ def dateline_fix(coords):
 
     Parameters
     ----------
-    coords : the coordinates to shift. An array of [lat, lon]
+    coords : List[float] of shape (n,) with n = amount of points
+        The coordinates to shift. An array of lat/lon points
 
     Returns
     -------
-    shifted_coords : the original coordinates shifted by 360 in latitude
+    shifted_coords : List[float] of shape (n,) with n = amount of points
+        The original coordinates shifted by 360 in latitude
     '''
     for i in range(len(coords)):
         coord = coords[i]
@@ -121,19 +134,23 @@ def dateline_fix(coords):
     return coords
 
 
-def get_enclosing_triangle(line_point, ico_points):
+def get_enclosing_triangle(line_point: List[float],
+                           ico_points: List[List[float]]) -> List[List[float]]:
     '''
-    Gets the indices of the vertices on the icosphere forming a
+    Gets the vertices on the icosphere forming a
     triangle enclosing the line point
 
     Parameters
     ----------
-    line_point : the point to get the enclosing triangle of ([x, y, z])
-    ico_points : all the vertices of the ico sphere ([[x, y, z]...])
+    line_point : List[float] of shape (3,)
+        The 3D point to get the enclosing triangle of (x, y, z)
+    ico_points : List[List[float]] of shape (n, 3) with n = verts in icosphere
+        All the vertices of the ico sphere
 
     Returns
     -------
-    closest : the indices of the 3 closest points on the icosphere to the
+    closest : List[List[float]] of shape (3, 3)
+        The coordinates of the 3 closest points on the icosphere to the
         line point. These 3 points form the triangle enclosing the point
     '''
     dist_sqrd = np.sum((ico_points - line_point)**2, axis=1)
@@ -142,44 +159,72 @@ def get_enclosing_triangle(line_point, ico_points):
     return ico_points[sort_indices[:3]]
 
 
-def subdivide_triangle(ps):
+def subdivide_triangle(ps: List[List[float]]) -> List[List[float]]:
     '''
     Subdivides a triangle once creating three new points, one
     on the midpoint on all three sides of the triangle
 
     Parameters
     ----------
-    ps : the points making up the triangle
+    ps : List[List[float]] of shape (3, 3)
+        The points making up the triangle
 
     Returns
     -------
-    [p_1, p_2, p_3] : 3 new points which subdivides the original triangle
+    subdivided_points : List[List[float]] of shape (3, 3)
+        3 new points which subdivides the original triangle
     '''
 
-    p_1 = [(ps[0][0] + ps[1][0]) / 2, (ps[0][1] + ps[1][1]) / 2, (ps[0][2] + ps[1][2]) / 2]
-    p_2 = [(ps[0][0] + ps[2][0]) / 2, (ps[0][1] + ps[2][1]) / 2, (ps[0][2] + ps[2][2]) / 2]
-    p_3 = [(ps[1][0] + ps[2][0]) / 2, (ps[1][1] + ps[2][1]) / 2, (ps[1][2] + ps[2][2]) / 2]
+    p_1 = [(ps[0][0] + ps[1][0]) / 2,
+           (ps[0][1] + ps[1][1]) / 2,
+           (ps[0][2] + ps[1][2]) / 2]
+
+    p_2 = [(ps[0][0] + ps[2][0]) / 2,
+           (ps[0][1] + ps[2][1]) / 2,
+           (ps[0][2] + ps[2][2]) / 2]
+
+    p_3 = [(ps[1][0] + ps[2][0]) / 2,
+           (ps[1][1] + ps[2][1]) / 2,
+           (ps[1][2] + ps[2][2]) / 2]
 
     return [normalize_point(p_1), normalize_point(p_2), normalize_point(p_3)]
 
 
-def normalize_point(p):
-    return p / np.linalg.norm(p)
-
-
-def generate_plot(simstart, time_offset, show=False):
+def normalize_point(p: List[List[float]]) -> List[List[float]]:
     """
-    Generates a plot of the lines from a given simulation start and a time offset.
-    The plot will be saved as a svg file with this naming convention:
-        <simstart>_<time_offset>h.svg
+    Normalizes the given point making its length equal 1
 
     Parameters
     ----------
-    simstart : the simulation start. Must be already downloaded.
+    p : List[List[float]] of shape (n,) with n = dimension of the point
+        The point to normalize
+
+    Return
+    ------
+    normalized_p : List[List[float]] of shape (n,) with
+                    n = dimension of the point
+        The normalized point
+    """
+
+    return p / np.linalg.norm(p)
+
+
+def generate_plot(simstart: str, time_offset: int, show: bool = False):
+    """
+    Generates a plot of the lines from a given simulation start and a
+    time offset. The plot will be saved as a svg file with this naming
+    convention: <simstart>\_<time\_offset>h.svg
+
+    Parameters
+    ----------
+    simstart : str
+        The simulation start. Must be already downloaded.
         Format: YYYYDDMMHH
-    time_offset : the time offset from the simulation start. In hours
-    show : if show is set to "True" then the plot will only be
-        shown and not saved
+    time_offset : int
+        The time offset from the simulation start. In hours
+    show : bool
+        If show is set to True then the plot will only be
+        shown and not saved. Defaults to False
     """
 
     lines = get_all_lines(simstart, time_offset)
@@ -241,13 +286,14 @@ def generate_plot(simstart, time_offset, show=False):
     plt.close()
 
 
-def generate_all_plots(simstart):
+def generate_all_plots(simstart: str):
     """
     Generates and saves the plots for each time offset for the given simstart
 
     Parameters
     ----------
-    simstart : the simulation start. Must be already downloaded.
+    simstart : str
+        The simulation start. Must be already downloaded.
         Format: YYYYDDMMHH
     """
     i = 0
