20cf5ab0babf87012754d840f3fcd971f20ce09a
Kaspar Moberg <kaspeti123@gmail.com>
2025-02-24 22:50:29 +0100

    refactor: moved visualization code to own file

--- src/main.py
+++ src/main.py
@@ -13,18 +13,12 @@ from dataclasses import dataclass
 import argparse
 from typing import Literal
 
-from coords import Coord3D
 from line_reader import Line, get_all_lines
 from multiscale import IcoPoint, multiscale
+from visualization import plot_map, plot_3D
 
-import numpy as np
-import pandas as pd
-import geopandas as gpd # type: ignore
-from shapely.geometry import LineString, Point
-from matplotlib.axes import Axes
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs  # type: ignore
-import cartopy.feature as cfeature # type: ignore
 
 
 @dataclass
@@ -42,58 +36,6 @@ class Data:
     lines: list[Line]
     ico_points_ms: dict[int, IcoPoint]
     line_points_ms: dict[str, dict[int, dict[int, tuple[int, float]]]]
-
-
-def plot_map(lines: list[Line], ax: Axes, ico_points: dict[int, IcoPoint] | None = None, show_ico: bool = False):
-    ids = [line.id for line in lines]
-    df = pd.DataFrame(ids, columns=["id"])  
-    geometry = [LineString([coord.to_list() for coord in line.coords]) for line in lines]
-    gdf = gpd.GeoDataFrame(df, geometry=geometry, crs="EPSG:4326")  # type: ignore
-    gdf.plot(ax=ax, transform=ccrs.PlateCarree(), linewidth=1, color="#0000ff")
-
-    ax.add_feature(cfeature.LAND, facecolor="white", edgecolor="black")   # type: ignore
-    ax.add_feature(cfeature.OCEAN, facecolor="lightgrey")     # type: ignore 
-    ax.add_feature(cfeature.BORDERS, linestyle=':', edgecolor="darkgrey")    # type: ignore
-
-    if show_ico:
-        if not ico_points:
-            print("'show_ico' was set to True for 'plot_map' but 'ico_points' was not set")
-            return
-
-        geo_points = []
-        for point in ico_points.values():
-            n = point.coord_3D.to_ndarray() / np.linalg.norm(point.coord_3D.to_ndarray())
-            geo_points.append(Coord3D(n[0], n[1], n[2]).to_lon_lat().to_list())
-
-        geometry = [Point(pt) for pt in geo_points]
-        gdf = gpd.GeoDataFrame(pd.DataFrame(), geometry=geometry, crs="EPSG:4326")  # type: ignore
-        gdf.plot(ax=ax, transform=ccrs.PlateCarree(), color="#ff0000", zorder=100, markersize=1)
-
-
-def plot_3D(lines: list[Line], ax: Axes, ico_points: dict[int, IcoPoint] | None = None, show_ico: bool = False):
-    u = np.linspace(0, 2 * np.pi, 20)
-    v = np.linspace(0, np.pi, 20)
-    x = 0.99 * np.outer(np.cos(u), np.sin(v))
-    y = 0.99 * np.outer(np.sin(u), np.sin(v))
-    z = 0.99 * np.outer(np.ones(np.size(u)), np.cos(v))
-
-    ax.plot_wireframe(x, y, z, color='gray', alpha=0.3)    # type: ignore
-
-    for line in lines:
-        xs = [coord.to_3D().x for coord in line.coords]
-        ys = [coord.to_3D().y for coord in line.coords]
-        zs = [coord.to_3D().z for coord in line.coords]
-        ax.plot(xs, ys, zs, color="blue")
-
-    if show_ico:
-        if not ico_points:
-            print("'show_ico' was set to True for 'plot_map' but 'ico_points' was not set")
-            return
-
-        xs = [pt.coord_3D.x for pt in ico_points.values()] 
-        ys = [pt.coord_3D.y for pt in ico_points.values()] 
-        zs = [pt.coord_3D.z for pt in ico_points.values()] 
-        ax.scatter(xs, ys, zs, color='red')
 
 
 def init() -> tuple[Settings, Data]:
@@ -118,6 +60,7 @@ def init() -> tuple[Settings, Data]:
     data = Data(lines=lines, ico_points_ms=ico_points_ms, line_points_ms=line_points_ms)
 
     return settings, data
+
 
 if __name__ == "__main__":
     settings, data = init()
